05396e1e024c7d2a1ffaafa73273815fec889e13
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-24 23:46:24 -0800

    set up Radix UI

--- task-manager/app/tasks/page.tsx
+++ task-manager/app/tasks/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes';
 
 const TasksPage = () => {
     return (
         <div>
-            TasksPage
+            <Button>New Task</Button>
         </div>
     )
 }
