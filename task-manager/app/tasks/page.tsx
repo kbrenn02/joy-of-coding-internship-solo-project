77bfea57ef796d47c3dea42e7178236c9e8282c9
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-30 23:57:43 -0800

    Comments about circular errors

--- task-manager/app/tasks/page.tsx
+++ task-manager/app/tasks/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+/* currently getting circular errors. If I don't have "use client", I get the following error
+        "You're importing a component that needs useState. It only works in a Client Component, 
+        but none of its parents are marked with "use client", so they're Server Components by default."
+    but if I do have "use client", I get:
+        "Error: async/await is not yet supported in Client Components, only Server Components. This error 
+        is often caused by accidentally adding `'use client'` to a module that was originally written for the server."
+*/
+
 import React, { useState } from 'react'
 import { Button, Table, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
