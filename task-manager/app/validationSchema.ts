202ef8e252cb1167b2ac4d8f585e2b5389aeca15
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-01 23:15:48 -0800

    completed remaining route methods

--- task-manager/app/validationSchema.ts
+++ task-manager/app/validationSchema.ts
@@ -1,10 +1,23 @@
 import { z } from 'zod';
 
+enum Status {
+    OPEN = "OPEN",
+    IN_PROGRESS = "IN PROGRESS",
+    CLOSED = "CLOSED",
+  }
+
 export const createTaskSchema = z.object ({
     title: z.string().min(1, 'Title is required.').max(255),
     description: z.string().min(1, 'Description is required'),
     due: z.string().min(1, 'Due date for the task is required. This can be changed later.').transform((str) => new Date()) // point 1
 });
+
+export const patchTaskSchema = z.object ({
+    title: z.string().min(1, 'Title is required.').max(255),
+    description: z.string().min(1, 'Description is required'),
+    due: z.string().min(1, 'Due date for the task is required. This can be changed later.').transform((str) => new Date()),
+    status: z.nativeEnum(Status)
+})
 
 /* 
 1.  When trying to post an example task, there was a continual error about submitting a date type. Unable to figure out
