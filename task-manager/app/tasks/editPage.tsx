f6eacae062bb7c573276af5da67232ef9e392fcb
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-05 22:27:58 -0800

    minor cleaning up

--- task-manager/app/tasks/editPage.tsx
+++ task-manager/app/tasks/editPage.tsx
@@ -48,6 +48,8 @@ const EditPage = () => {
 		}
 	};
 
+
+    // i can update title or update title and description, but not description alone
     const updateTask = async (index: number) => {
 		const dbData = await axios.get(`/api/tasks/${tasks[index].id}`)
         const task = dbData.data // the task data from the database is now assigned to the const task
@@ -55,7 +57,7 @@ const EditPage = () => {
         if (isediting) {
             const data = {
                 title,
-                description
+                description,
             }
             axios.patch(`/api/tasks/${tasks[index].id}`, data).then(response => console.log("success"))
             .catch((error) => console.error("There was an error updating the task", error)
@@ -65,21 +67,21 @@ const EditPage = () => {
     };
 
 //Edit Field function to handle edits
-    const EditField = ({value, fieldType, handleChange}:{value : any, fieldType : any, handleChange : (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
-        return (
-            <input
-              id="status"
-              type = {fieldType}
-              onChange={handleChange}
-              defaultValue={value}>
-            </input>
-        )
-    }
+    // const EditField = ({value, fieldType, handleChange}:{value : any, fieldType : any, handleChange : (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
+    //     return (
+    //         <input
+    //           id="status"
+    //           type = {fieldType}
+    //           onChange={handleChange}
+    //           defaultValue={value}>
+    //         </input>
+    //     )
+    // }
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const value = e.target.value
 
-    }
+    // }
     // {isediting ? <EditField value={task.status} fieldType="string" handleChange={handleChange}/> : <span></span>
     //
 
@@ -96,7 +98,6 @@ const EditPage = () => {
                     </Table.RowHeaderCell>
                     <Table.Cell>
                         {task.status}
-                        {index} {/* Will need to remove this */}
                     </Table.Cell>
                     <Table.Cell>
                         {/* {task.title} */}
@@ -111,7 +112,7 @@ const EditPage = () => {
                     <Table.Cell>
                     { isediting ? 
                         <span>
-                            <input placeholder={task.description} onChange = {(e) => (setTitle(e.target.value))}/>
+                            <input placeholder={task.description} onChange = {(e) => (setDescription(e.target.value))}/>
                         </span> : <span>{task.description}</span>
                         }
                     </Table.Cell>
@@ -134,78 +135,7 @@ const EditPage = () => {
             ))}
         </Table.Body>
     )
-        }
-
-
-
-
-//     return (
-//         <>
-//             <div className='ml-4'>
-//                 <Button><Link href='/tasks/new'>New Task</Link></Button>
-//             </div>
-
-//             <Table.Root className='mt-4'>
-
-//                 {/* tasks.map(task) is a function that goes through every line of data. Task.XXX calls the data
-//                 with that specific header. This function has an issue showing date types, so task.due.toDateString() 
-//                 is needed to turn the date type to a string type*/}
-//                 <Table.Body>
-//                     {tasks.map((task) =>
-//                         <Table.Row>
-//                             <Table.RowHeaderCell>
-//                                 <input
-//                                   type="date"
-//                                   id="due"
-//                                   defaultValue={task.due.toDateString()}>
-//                                 </input>
-//                             </Table.RowHeaderCell>
-//                             {/* <Table.Cell className='columns'>
-//                                 <input type="radio" name="radio">OPEN</input>
-//                                 <input type="radio" name="radio">IN PROGRESS</input>
-//                                 <input type="radio" name="radio">CLOSED</input>
-//                             </Table.Cell> */}
-//                             {/* <Table.RowHeaderCell>{task.due.toDateString()}</Table.RowHeaderCell> */}
-//                             <Table.Cell>
-//                                 <input
-//                                     type="text"
-//                                     id="status"
-//                                     defaultValue={task.status}>
-//                                 </input>
-//                             </Table.Cell>
-//                             <Table.Cell>
-//                                 <input
-//                                     type="text"
-//                                     id="title"
-//                                     defaultValue={task.title}>
-//                                 </input>
-//                             </Table.Cell>
-//                             <Table.Cell>
-//                                 <input
-//                                     type="text"
-//                                     id="description"
-//                                     defaultValue={task.description}>
-//                                 </input>
-//                             </Table.Cell>
-//                             <Table.Cell>
-//                                 <Flex gap="2">
-//                                     <Button 
-//                                       className='w-1/2'>
-//                                         Save
-//                                     </Button>
-//                                     <Button className='w-1/2' color="red">
-//                                         Delete
-//                                     </Button>
-//                                 </Flex>
-//                             </Table.Cell>
-//                         </Table.Row>
-//                     )}
-//                 </Table.Body>
-//             </Table.Root>
-
-//         </>
-//     )
-// }
+}
 
 
 export default EditPage
