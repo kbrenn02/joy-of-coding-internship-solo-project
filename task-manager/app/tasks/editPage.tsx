625840f240755a876e59745e00cf5be7008f118c
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-06 19:27:05 -0800

    Moved delete button and cleared excess comments

--- task-manager/app/tasks/editPage.tsx
+++ task-manager/app/tasks/editPage.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 import prisma from '@/prisma/client';
 import axios from 'axios';
 import { EnumValues, object } from 'zod';
-import { useRouter } from 'next/navigation';
 import TaskRow from './taskRow';
 
 
@@ -41,55 +40,25 @@ const EditPage = () => {
 
     // console.log(tasks[0])
 
-    // this works as a delete function. Was having issues with the refresh, so I had it send the user to the homepage
-    // after deleting a task
-    const router = useRouter();
-    const removeTask = async (index: number) => {
-		try {
-			await axios.delete(`/api/tasks/${tasks[index].id}`);
-			router.refresh();
-            router.push('/');
-		} catch (error) {
-			console.error('The following error occurred:', error);
-		}
-	};
+    
 
-
-    // i can update title or update title and description, but not description alone
-    const updateTask = async (index: number) => {
-		const dbData = await axios.get(`/api/tasks/${tasks[index].id}`)
-        const task = dbData.data // the task data from the database is now assigned to the const task
+    // this has been moved to the taskRow.tsx doc
+    // // i can update title or update title and description, but not description alone
+    // const updateTask = async (index: number) => {
+	// 	const dbData = await axios.get(`/api/tasks/${tasks[index].id}`)
+    //     const task = dbData.data // the task data from the database is now assigned to the const task
         
-        if (isediting) {
-            const data = {
-                title,
-                description,
-            }
-            axios.patch(`/api/tasks/${tasks[index].id}`, data).then(response => console.log("success"))
-            .catch((error) => console.error("There was an error updating the task", error)
-            )
-        }
-        setIsEditing(!isediting)
-    };
-
-//Edit Field function to handle edits
-    // const EditField = ({value, fieldType, handleChange}:{value : any, fieldType : any, handleChange : (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
-    //     return (
-    //         <input
-    //           id="status"
-    //           type = {fieldType}
-    //           onChange={handleChange}
-    //           defaultValue={value}>
-    //         </input>
-    //     )
-    // }
-
-    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     const value = e.target.value
-
-    // }
-    // {isediting ? <EditField value={task.status} fieldType="string" handleChange={handleChange}/> : <span></span>
-    //
+    //     if (isediting) {
+    //         const data = {
+    //             title,
+    //             description,
+    //         }
+    //         axios.patch(`/api/tasks/${tasks[index].id}`, data).then(response => console.log("success"))
+    //         .catch((error) => console.error("There was an error updating the task", error)
+    //         )
+    //     }
+    //     setIsEditing(!isediting)
+    // };
 
     return (
         <Table.Body>
