ba3a310a5f96d39abdc9a0e971e3ab9497adf4b7
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-16 17:03:32 -0800

    Structure of home page: two columns for information

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -5,6 +5,8 @@ import { Flex } from "@radix-ui/themes";
 export default function Home() {
   return (
     <>
+    {/* Need to center the title "welcome to task manager" */}
+        <div className="container bg-center">Welcome to the Task Manager</div>
         <Flex gap="2" className="w-full">
             <div className="bg-amber-400 w-1/2">
                 Div 1
