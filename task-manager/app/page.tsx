c1ccc7410a45c739d3c0775af6a8e0cf6f949480
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-18 23:29:44 -0800

    Home page layout complete

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -4,41 +4,17 @@ import { Flex } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen">
     {/* Need to center the title "welcome to task manager" */}
-        <div className="flex justify-center">Welcome to the Task Manager</div>
-        <Flex gap="2" className="w-full">
-            <div className="bg-red-400 w-1/2 h-96">
+        <div className="flex justify-center h-16 text-3xl font-extrabold">Welcome to the Task Manager</div>
+        <Flex gap="2" className="w-full h-full">
+            <div className="bg-red-400 w-1/2 h-full border border-gray-light rounded-xl shadow">
                 Div 1
             </div>
-            <div className="bg-orange-400 w-1/2 h-96">
+            <div className="bg-orange-400 w-1/2 h-full border border-gray-light rounded-xl shadow">
                 Div 2
             </div>
-        </Flex>
-        <Flex gap="2" className="w-full">
-            <div className="bg-yellow-400 w-1/2 h-96">
-                Div 3
-            </div>
-            <div className="bg-green-400 w-1/2 h-96">
-                Div 4
-            </div>
-        </Flex>
-        <Flex gap="2" className="w-full">
-            <div className="bg-blue-400 w-1/2 h-96">
-                Div 5
-            </div>
-            <div className="bg-purple-400 w-1/2 h-96">
-                Div 6
-            </div>
-        </Flex>
-        <Flex gap="2" className="w-full">
-            <div className="bg-pink-400 w-1/2 h-96">
-                Div 7
-            </div>
-            <div className="bg-gray-400 w-1/2 h-96">
-                Div 8
-            </div>
-        </Flex>
-    </>
+        </Flex>       
+    </div>
   );
 }
