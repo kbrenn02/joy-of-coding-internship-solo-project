163e1c8a345f6a528884520d85786cbb556a2999
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-26 22:02:21 -0800

    Adding a component to count tasks by status

---
+++ task-manager/app/CountTasks.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+const CountTasks = () => {
+  return (
+    <div>CountTasks</div>
+  )
+}
+
+export default CountTasks
--- task-manager/app/DataChart.tsx
+++ task-manager/app/DataChart.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Chart from 'chart.js/auto';
 import axios from 'axios';
 import { EnumValues } from 'zod';
+import prisma from '@/prisma/client';
 
 
 interface TaskForm {
@@ -13,12 +14,9 @@ interface TaskForm {
 }
 
 
-const DataChart = () => {
+const DataChart = async() => {
 
     const [data, setData] = useState<TaskForm[]>([]);
-    const [open, setOpen] = useState(0);
-    const [inProgress, setInProgress] = useState(0);
-    const [closed, setClosed] = useState(0);
 
     useEffect(() => {
         const getData = async() => {
@@ -35,72 +33,10 @@ const DataChart = () => {
         getData();
     }, []);
 
-    // BY RUNNING JUST THE IF STATEMENT, IT CAUSES AN INFINITE LOOP
-    // useEffect(() => {
-        // if (data.length > 0) {
-    const statuses = data.map((task) => task.status.toString())
-        //         const chartData = () => {
-        //             let i=0;
-        //             while (i<statuses.length) {
-        //                 if (statuses[i] == "OPEN") {
-        //                     setOpen(open+1);
-        //                 } else if (statuses[i] == "IN_PROGRESS") {
-        //                     setInProgress(inProgress+1);
-        //                 } else if (statuses[i] == "CLOSED") {
-        //                     setClosed(closed+1)
-        //                 }
-        //                 i++;
-        //                 console.log("current i value", i)
-        //             }
-        //         }
-        // chartData();
-
-    // Attempt at incrementer. Need to ask Suzanne
-    const handleIncrement = () => {
-        statuses.forEach(item => {
-              if (item === "OPEN") {
-                setOpen(open + 1); // Increment count by 1
-              } else if (item === "IN_PROGRESS") {
-                setInProgress(inProgress + 1);
-              } else if (item === "CLOSED"){
-                setClosed(closed + 1);
-              }
-            });
-          };
-        
-          // Call handleIncrement function when component renders
-    useEffect(() => {
-        handleIncrement();
-    }, []);
-        //     console.log("status1", statuses)
-        // }
-    // }, [data]);
-
-    console.log("statuses", statuses)
-    console.log("statuses length", statuses.length)
-    console.log("statuses1", statuses[1])
-    console.log("open1", open)
-    console.log("inProgress1", inProgress)
-    console.log("closed1", closed)
+    console.log(data)
 
     useEffect(() => {
         if (data.length > 0) {
-            // const ids = data.map((task) => task.id);
-            // const statuses = data.map((task) => task.status.toString())
-
-            // for (let i=0; i<statuses.length; i++) {
-            //     if (statuses[i] == "OPEN") {
-            //         setOpen(open+1);
-            //     } else if (statuses[i] == "IN_PROGRESS") {
-            //         setInProgress(inProgress+1);
-            //     } else if (statuses[i] == "CLOSED") {
-            //         setClosed(closed+1)
-            //     }
-            // }
-
-            // console.log("open2", open)
-            // console.log(inProgress)
-            // console.log(closed)
 
             const ctx = document.getElementById('myChart') as HTMLCanvasElement;
 
@@ -109,7 +45,7 @@ const DataChart = () => {
                 data: {
                     labels: ["Open", "In Progress", "Closed"],
                     datasets: [{
-                        data: [open, inProgress, closed]
+                        data: [1, 2, 3]
                         // FIGURE OUT HOW TO ADD THE COUNT OF EACH TASK TYPE TO THE LABEL
                         // backgroundColor: 'rgba(75, 192, 192, 0.2)',
                         // borderColor: 'rgba(75, 192, 192, 1)',
@@ -119,9 +55,6 @@ const DataChart = () => {
             });
         }
     }, [data]);
-
-    
-    
 
 
     return (
