5c01bf28277e4fe2e8f675b9405b78e28faf36b1
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-02-27 16:36:02 -0800

    Using Javascript filter function, was able to filter list of tasks based on title

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -42,7 +42,55 @@ export default function Home() {
         tasks.sort(sortByDateAsc);};
 
     listOfTasks();
-    console.log(tasks[0].title)
+    console.log(tasks)
+
+    let openCount = 0
+    let notOpen = 0
+
+    function filterByStatus(task: any) {
+        if (Number.isFinite(task.id) && task.title === "Task 2"){
+            openCount++;
+            return true
+        } else {
+            notOpen++;
+            return false
+        }
+    }
+
+    const tasksByID = tasks.filter(filterByStatus);
+    console.log(tasksByID)
+    console.log(typeof(tasks))
+    
+    console.log(tasks.filter(filterByStatus))
+    
+
+    const arr = [
+        { id: 15, pop: "hello" },
+        { id: -1, pop: "bye" },
+        { id: 0, pop: "hi" },
+        { id: 3, pop: "hey" },
+        { id: 12.2, pop: "he" },
+        {},
+        { id: null },
+        { id: NaN },
+        { id: "undefined" },
+      ];
+
+      console.log(arr)
+      console.log(typeof(arr))
+      
+      let invalidEntries = 0;
+      
+      function filterByID(item:any) {
+        if (Number.isFinite(item.id) && item.id !== 0 && item.pop === "hey") {
+          return true;
+        }
+        invalidEntries++;
+        return false;
+      }
+      
+      const arrByID = arr.filter(filterByID);
+      console.log(arrByID)
 
     return (
         <div className="h-screen">
